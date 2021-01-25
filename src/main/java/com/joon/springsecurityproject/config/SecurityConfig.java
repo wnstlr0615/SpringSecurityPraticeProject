f08f6964b60cf02a09e18805a62828e1f2fffff9
wnstlr0615 <ryan0@kakao.com>
2021-01-25 18:30:42 +0900

    스프링 시큐리티 커스터 마이징 :인메모리 유저 추가

--- src/main/java/com/joon/springsecurityproject/config/SecurityConfig.java
+++ src/main/java/com/joon/springsecurityproject/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.joon.springsecurityproject.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -8,6 +9,17 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception { // 사용자 추가 기능 메소드
+        auth.inMemoryAuthentication()
+                .withUser("joon").password("{noop}123").roles("USER")// {noop} Spring 5부터 사용가능 한 기본 비밀번호 인코더(암호화 x)
+                .and()
+                .withUser("admin").password("{noop}!@#").roles("ADMIN");//암호화 해서 전송
+
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()  //접근 설정
@@ -16,6 +28,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated(); //로그인 사용자는 가능
         http.formLogin();
         http.httpBasic();
-        ;
     }
+
 }
