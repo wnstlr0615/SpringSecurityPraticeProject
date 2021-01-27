bbb2986e9b9d97ceb1cbdc409539fdb09bec07df
wnstlr0615 <ryan0@kakao.com>
2021-01-27 14:22:45 +0900

    21.	스프링 시큐리티 ignoring() 1부

--- src/main/java/com/joon/springsecurityproject/config/SecurityConfig.java
+++ src/main/java/com/joon/springsecurityproject/config/SecurityConfig.java
@@ -1,24 +1,50 @@
 package com.joon.springsecurityproject.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.access.AccessDecisionManager;
-import org.springframework.security.access.AccessDecisionVoter;
 import org.springframework.security.access.expression.SecurityExpressionHandler;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
-import org.springframework.security.access.vote.AffirmativeBased;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
-import org.springframework.security.web.access.expression.WebExpressionVoter;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+
+    public SecurityExpressionHandler expressionHandler(){ // // 권한 세부 설정
+       RoleHierarchyImpl roleHierarchy=new RoleHierarchyImpl();
+       roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
+       DefaultWebSecurityExpressionHandler handler=new DefaultWebSecurityExpressionHandler();
+       handler.setRoleHierarchy(roleHierarchy);
+        return handler;
+   }
+
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        //web.ignoring().mvcMatchers("/favicon.ico");
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());// favicon.ico 등 리소스 허용
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()  //접근 설정
+                .mvcMatchers("/", "/info", "/account/**").permitAll() //모두 허용
+                .mvcMatchers("/admin").hasRole("ADMIN") //자격 요건
+                .mvcMatchers("/user").hasRole("USER") //자격 요건
+                .anyRequest().authenticated()//로그인 사용자는 가능
+                .expressionHandler(expressionHandler())
+            // .accessDecisionManager(accessDecisionManager())
+        ;
+        http.formLogin();
+        http.httpBasic();
+    }
+
 
 
    /* @Override
@@ -41,25 +67,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         List<AccessDecisionVoter<? extends Object>> voters= Arrays.asList(webExpressionVoter);
         return new AffirmativeBased(voters);
     }*/
-   public SecurityExpressionHandler expressionHandler(){ // // 권한 세부 설정
-       RoleHierarchyImpl roleHierarchy=new RoleHierarchyImpl();
-       roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
-       DefaultWebSecurityExpressionHandler handler=new DefaultWebSecurityExpressionHandler();
-       handler.setRoleHierarchy(roleHierarchy);
-        return handler;
-   }
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()  //접근 설정
-                .mvcMatchers("/", "/info", "/account/**").permitAll() //모두 허용
-                .mvcMatchers("/admin").hasRole("ADMIN") //자격 요건
-                .mvcMatchers("/user").hasRole("USER") //자격 요건
-                .anyRequest().authenticated()//로그인 사용자는 가능
-               // .accessDecisionManager(accessDecisionManager())
-                .expressionHandler(expressionHandler())
-        ;
-        http.formLogin();
-        http.httpBasic();
-    }
 
 }
